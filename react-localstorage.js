f90f892ec0a15be660b2f767ca43d331084dac30
Dmitry <null.none@yandex.ru>
2016-11-20 13:08:56 +0200

    version 0.0.1

---
+++ react-localstorage.js
@@ -0,0 +1,21 @@
+const reactLocalStorage = {
+    set: function(key, value) {
+        localStorage[key] = value;
+        return localStorage[key];
+    },
+    get: function(key, defaultValue) {
+        return localStorage[key] || defaultValue;
+    },
+    setObject: function(key, value) {
+        localStorage[key] = JSON.stringify(value);
+        return localStorage[key];
+    },
+    getObject: function(key, value) {
+        return JSON.parse(localStorage[key] || '{}');
+    },
+    clear: function() {
+        return localStorage.clear();
+    }
+}
+
+export default reactlocalStorage;
