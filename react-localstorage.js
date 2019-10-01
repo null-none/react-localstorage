8028a74b2effb01f12c600620cc597e1d54b6fd1
Faisal Manzer <faisal_manzer@yahoo.in>
2019-10-01 15:21:11 +0530

    Added more examples in README Added silent to get and getObject Added support for defaultValue

--- react-localstorage.js
+++ react-localstorage.js
@@ -3,20 +3,34 @@ exports.reactLocalStorage = {
         localStorage[key] = value;
         return localStorage[key];
     },
-    get: function(key, defaultValue) {
-        return localStorage[key] || defaultValue;
+    get: function(key, defaultValue=undefined, silent=true) {
+        var value = localStorage[key] || defaultValue;
+
+        // if silent=false throw error
+        if(!silent && !value)
+            throw key + " not found in localStorage";
+
+        return value;
     },
     setObject: function(key, value) {
         localStorage[key] = JSON.stringify(value);
         return localStorage[key];
     },
-    getObject: function(key, value) {
-        return JSON.parse(localStorage[key] || '{}');
+    getObject: function(key, defaultValue={}, silent=true) {
+        value = this.get(key, JSON.stringify(defaultValue), silent);
+
+        try{
+            return JSON.parse(value);
+        } catch(e) {
+            // will raise error for parsing
+            if(!silent)
+                throw 'Error in parsing value'
+        }
     },
     clear: function() {
         return localStorage.clear();
     },
     remove: function(key) {
         return localStorage.removeItem(key);
-    }
+    },
 }
